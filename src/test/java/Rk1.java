a9a5ca50ec257b3e3e0ebf6b45314bb444bb5ab4
Rakesh <rakeshkashameeni@gmail.com>
2022-02-13 13:38:49 +1100

    sampleTest

--- src/test/java/Rk1.java
+++ src/test/java/Rk1.java
@@ -1,6 +1,4 @@
-import org.junit.platform.engine.support.descriptor.FileSystemSource;
-
-public class Rk {
+public class Rk1 {
      public static void main(String[] args) {
         System.out.println("Hello World");
 //
